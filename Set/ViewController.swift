8e0ad432a7c0df81e202ed14fa114631c921e6bf
Anna Belousova <anna.belousova.2011@gmail.com>
2021-06-03 14:17:17 +0400

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Anna Belousova on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
